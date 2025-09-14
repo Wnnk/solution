097e2550275198474af7bab5d8397db56e6fe231
Wnnk <1275056222@qq.com>
2025-09-14 20:26:08 +0800

    添加固定列

--- src/components/Table/SuperTable/SuperTable.vue
+++ src/components/Table/SuperTable/SuperTable.vue
@@ -10,36 +10,50 @@
           :key="tableKey"
           :row-style="{ height: rowHeight + 'px' }"
           @header-click="selectColumn"
+          :header-cell-class-name="changeColumnStyle"
           @cell-click="selectCell"
-          
+          :cell-style="changeCellStyle"
         >
           <el-table-column>
             <template #header>
               <TableHeader />
             </template>
-            <el-table-column
-            v-for="column in renderColumns"
-            :key="column.key"
-            :prop="column.prop"
-            :width="column.width"
-            :type="column.type"
-            :fixed="column.fixed"
-          >
-            <template #header>
-              <!-- 组件默认表头布局 -->
-              <div class="header-wrapper" >
-                <i class="iconfont icon-drag" :style="{ opacity: isDraggable? 1 : 0 }"></i>
-                <div>{{ column.label }}</div>
-                <div @click.stop="openPopover">
-                  <TablePopover :column="_.cloneDeep(column)">
-                    <i class="iconfont icon-header-detail"></i>
-                  </TablePopover>
-                </div>
+            <el-table-column>
+              <div class="">
+                <div class="detail-icon"></div>
+                <div class="add-child-icon"></div>
               </div>
-            </template>
+            </el-table-column>
+            <!-- 动态列 -->
+            <el-table-column
+              v-for="column in renderColumns"
+              :key="column.key"
+              :prop="column.prop"
+              :width="column.width"
+              :type="column.type"
+              :fixed="column.fixed"
+            >
+              <template #header>
+                <!-- 组件默认表头布局 -->
+                <div class="header-wrapper">
+                  <i class="iconfont icon-drag" :style="{ opacity: isDraggable ? 1 : 0 }"></i>
+                  <div>{{ column.label }}</div>
+                  <div @click.stop="openPopover">
+                    <TablePopover :column="_.cloneDeep(column)">
+                      <i class="iconfont icon-header-detail"></i>
+                    </TablePopover>
+                  </div>
+                </div>
+              </template>
+              <!-- <template #default="{ row }" v-if="column.type === 'default' || !column.type">
+                <div>
+                  <span>{{ row[column.prop] }}</span>
+                  <div class="detail-icon">1</div>
+                </div>
+              </template> -->
             </el-table-column>
           </el-table-column>
-          
+
           <template #empty>
             <slot name="empty"></slot>
           </template>
@@ -55,8 +69,6 @@
         ></el-pagination>
       </el-footer>
     </el-container>
-
-   
   </div>
 </template>
 
@@ -65,8 +77,8 @@ import { ref, computed, provide, watch, onUnmounted, nextTick } from 'vue'
 import TableHeader from './components/TableHeader/TableHeader.vue'
 import { ColumnType, TableData, FilterStore } from './type'
 import TablePopover from './components/TablePopover/TablePopover.vue'
-import {  makeColumnsDraggable } from './Draggable'
-import _  from 'lodash'
+import { makeColumnsDraggable } from './Draggable'
+import _ from 'lodash'
 import {
   editColumn,
   hideColumn,
@@ -80,9 +92,6 @@ import {
 import { updateSortData } from './SortColumns'
 import { updateFilterData } from './FilterColumns'
 import { ElTable } from 'element-plus' // 导入类型
-
-
-
 
 const tableRef = ref<InstanceType<typeof ElTable> | null>(null)
 const tableKey = ref(0)
@@ -112,6 +121,7 @@ const openPopover = () => {
   handerPopoverVisible.value = true
 }
 
+/*****************************列拖拽相关*************************************  */
 /* 列源数据 */
 const orgincolumns = ref<ColumnType[]>([...props.columns])
 
@@ -119,7 +129,7 @@ const orgincolumns = ref<ColumnType[]>([...props.columns])
 const renderColumns = ref<ColumnType[]>([...props.columns])
 
 const isDraggable = ref(false)
-let destroyDrag : (() => void) | null = null
+let destroyDrag: (() => void) | null = null
 
 /* 作用于el-table的强制渲染 */
 watch(
@@ -134,28 +144,28 @@ watch(
 watch(
   () => isDraggable.value,
   (newVal) => {
-    if(destroyDrag){
-      destroyDrag();
-      destroyDrag = null;
+    if (destroyDrag) {
+      destroyDrag()
+      destroyDrag = null
     }
-    if(newVal){
+    if (newVal) {
       destroyDrag = makeColumnsDraggable(
         '.el-table__header-wrapper thead tr',
         orgincolumns.value,
         '.icon-drag',
         (newColumns) => {
           renderColumns.value = newColumns
-        }
+        },
       )
     }
-  }
+  },
 )
 // 组件卸载时确保清理
 onUnmounted(() => {
   if (destroyDrag) {
-    destroyDrag();
+    destroyDrag()
   }
-});
+})
 
 provide('useDraggable', {
   isDraggable,
@@ -163,7 +173,6 @@ provide('useDraggable', {
     isDraggable.value = value
   },
 })
-
 
 const emit = defineEmits(['update:columns', 'update:data'])
 
@@ -191,6 +200,7 @@ provide('useRowHeight', {
   },
 })
 
+/****************************条件过滤*****************************/
 /* 保存源数据 */
 const orginData = ref<TableData[]>([...props.data])
 /* 最终渲染数据 */
@@ -199,12 +209,16 @@ const renderData = ref<TableData[]>([...props.data])
 const processData = (data: TableData[], filterStore: FilterStore) => {
   let result = [...data]
   const { sort, filter } = filterStore
-  result = updateSortData(result, sort.columnKey, sort.order);
-  result = updateFilterData(props.columns, result, filter.columnKey, filter.parameterKey, filter.value);
+  result = updateSortData(result, sort.columnKey, sort.order)
+  result = updateFilterData(
+    props.columns,
+    result,
+    filter.columnKey,
+    filter.parameterKey,
+    filter.value,
+  )
   return result
 }
-
-
 
 const filterStore = ref<FilterStore>({
   sort: {
@@ -218,15 +232,15 @@ const filterStore = ref<FilterStore>({
   },
   group: {
     columnKey: '',
-  }
+  },
 })
 
 watch(
   () => filterStore.value,
-  (newVal:FilterStore) => {
+  (newVal: FilterStore) => {
     renderData.value = processData([...orginData.value], newVal)
   },
-  { immediate: true }
+  { immediate: true },
 )
 
 watch(
@@ -234,12 +248,8 @@ watch(
   (newData: TableData[]) => {
     orginData.value = [...newData]
     renderData.value = processData([...newData], filterStore.value)
-  }
+  },
 )
-
-
-
-
 
 provide('useData', {
   data: computed(() => props.data),
@@ -255,28 +265,53 @@ provide('useFilterStore', {
   },
 })
 
-
-
 /** *************************************选中事件 *******************************  */
-const activeColumn = ref<string | null>(null)
-const activeCell = ref<{row:any, column:any, cell:any} | null>(null)
-const selectColumn = (column:any, event?:Event) => {
+const activeColumn = ref<number | null>(null)
+const activeCell = ref<{ rowIndex: number; columnIndex: number } | null>(null)
+/********************************单元格选择****************************   */
+const selectCell = (row: any, column: any, cell: any, event?: Event) => {
+  activeColumn.value = null
+  const columnIndex = renderColumns.value.findIndex((item) => item.key === column.rawColumnKey)
+  const rowIndex = renderData.value.findIndex((item) => item.id === row.id)
+  activeCell.value = { rowIndex, columnIndex }
+}
+const changeCellStyle = (data: any) => {
+  if (activeCell.value === null) return {}
 
-
+  if (
+    data.rowIndex === activeCell.value.rowIndex &&
+    data.columnIndex === activeCell.value.columnIndex
+  ) {
+    return {
+      border: '1px solid #409eff',
+    }
+  }
+}
+/*********************************列选择*****************************   */
+const selectColumn = (column: any) => {
+  const columnIndex = renderColumns.value.findIndex((item) => item.key === column.rawColumnKey)
+  activeColumn.value = columnIndex
+  if (renderData.value.length > 0) {
+    activeCell.value = {
+      rowIndex: 0,
+      columnIndex: columnIndex,
+    }
+  } else {
+    activeCell.value = null
+  }
 }
 
-const selectCell = (row:any, column:any, cell:any, event?:Event) => {
-  cell.classList.add('active-cell')
-  console.log(cell)
+const changeColumnStyle = (data: any) => {
+  const columnIndex = data.columnIndex
+  // 为选中列的表头添加样式
+  if (activeColumn.value !== null && columnIndex === activeColumn.value) {
+    return 'active-column-header'
+  }
+
+  return ''
 }
-
-
 </script>
 
 <style lang="scss" scoped>
 @use './SuperTable.scss';
-::v-deep active-cell { 
-  background-color: #f5f5f5;
-  border: 1px solid red;
-}
 </style>
---
+++ src/components/Table/SuperTable/components/TableHeader/FillColor/FillColor.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-popover v-model:visible="visible" width="500" placement="bottom" trigger="click">
+    <div class="fill-color-popover">
+      <div class="fill-color-title">设置填色条件</div>
+      <div class="fill-color-content"></div>
+    </div>
+    <template #reference>
+      <slot></slot>
+    </template>
+  </el-popover>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+
+const visible = ref(false)
+</script>
+
+<style lang="scss" scoped></style>
